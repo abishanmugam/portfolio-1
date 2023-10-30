3aa70aef09d6d6cb191ec9a2130533d09b8cfe0e
Mukesh Selvaraj <abirami@ishamistygreen.com>
2023-10-30 16:41:43 +0530

    created src files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import 'boxicons';
+import ScrollUp from './ScrollUp';
+import Preloader from './Preloader';
+import styled, { ThemeProvider } from 'styled-components';
+import { themes } from './themes/theme';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [theme, setTheme] = useState('light');
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 7000);
+  }, []);
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading === false ? (
+        <ThemeProvider theme={themes[theme]}>
+          <Apps>
+            <Router>
+              <Switch>
+                <Route exact path="/">
+                  <Home theme={theme} setTheme={setTheme} />
+                  <ScrollUp />
+                </Route>
+              </Switch>
+            </Router>
+          </Apps>
+        </ThemeProvider>
+      ) : (
+        <Preloader />
+      )}
+    </>
   );
 }
 
 export default App;
+
+const Apps = styled.div`
+  background-color: ${(props) => props.theme.pageBackground};
+`;
